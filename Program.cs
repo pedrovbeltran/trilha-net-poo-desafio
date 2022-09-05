963958a1ad0b150266cadf6efde03ba97d088d71
Pedro Beltran <pedrovbeltran@gmail.com>
2022-09-05 07:22:50 -0300

    Making change requests and completing the challenge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokiaTijolao = new Nokia;
+Smartphone iphoneDeRico = new Iphone;
+
+nokiaTijolao.InstalarAplicativo();
+iphoneDeRico.InstalarAplicativo();
+
+nokiaTijolao.Ligar();
+nokiaTijolao.ReceberLigacao();
